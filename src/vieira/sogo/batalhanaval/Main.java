e35d5f881621741936c8554a37cef929d3e04687
Sogolon <sogolonmvj@yahoo.com>
2021-12-15 15:12:15 -0300

    Initial commit

---
+++ src/vieira/sogo/batalhanaval/Main.java
@@ -0,0 +1,15 @@
+package vieira.sogo.batalhanaval;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int[][] tabuleiro;
+        int navios;
+        int tiro;
+        int rodada;
+
+        tabuleiro = new int[10][10]; // tabuleiro 10x10
+        navios = new int[10][2]; // dez submarinos posicionados
+        tiro = new int[2]; // coordenadas do tiro
+    }
+}
