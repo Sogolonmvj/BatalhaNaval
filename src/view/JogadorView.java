3e6ecd9142a149fdd4da387597ff9d3aab5c52ec
Paulo Denadai <pdenadai@outlook.com>
2021-12-20 21:26:18 -0300

    Class Jogador Class JogadorView

--- src/view/JogadorView.java
+++ src/view/JogadorView.java
@@ -1,4 +1,37 @@
 package view;
 
+import domain.Jogador;
+import enums.TipoJogador;
+
+import java.util.Scanner;
+
 public class JogadorView {
+    private Scanner scanner;
+    private Jogador jogador1;
+    private Jogador jogador2;
+
+    public JogadorView() {
+        String name = this.askName();
+        this.jogador1 = new Jogador(name, TipoJogador.COMPUTADOR);
+        this.jogador2 = new Jogador("COMPUTADOR", TipoJogador.COMPUTADOR);
+    }
+
+    private String askName() {
+        System.out.println("Qual o nome do joador?");
+        System.out.println("#: ");
+        String name = scanner.nextLine();
+        if (name.isBlank()) {
+            System.out.println("Nome inválido!");
+            name = askName();
+        }
+        return name;
+    }
+
+    public Jogador getJogador1() {
+        return jogador1;
+    }
+
+    public Jogador getJogador2() {
+        return jogador2;
+    }
 }
